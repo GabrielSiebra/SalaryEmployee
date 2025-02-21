0dac69fb9bc191344216b52e3881a3cb2608d947
GabrielSiebra <gabrielsiebra2014@gmail.com>
2025-02-21 19:12:32 -0300

    Complement Employee

--- src/main/java/entities/Employee.java
+++ src/main/java/entities/Employee.java
@@ -5,4 +5,33 @@ public class Employee {
     private int id;
     private String name;
     private double salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+
+
+    }
 }
