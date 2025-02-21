dc9f69e0be77ae7b6e9c0ddf68f896073e12e45f
GabrielSiebra <gabrielsiebra2014@gmail.com>
2025-02-21 19:58:06 -0300

    list

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -19,7 +19,8 @@ public class Program {
 
     for (int i=0; i<N; i++){
 
-        System.out.print("Employee#" + (i+1)+ ":");
+        System.out.println();
+        System.out.println("Employee#" + (i+1)+ ":");
         System.out.print("Id: ");
         Integer id = sc.nextInt();
         System.out.print("Name: ");
@@ -28,16 +29,25 @@ public class Program {
         System.out.print("Salary: ");
         double salary = sc.nextDouble();
 
+        Employee emp = new Employee(id, name, salary);
+
+        list.add(emp);
     }
+        System.out.print("Enter the employee id that will have salary increase: ");
+        int idsalary= sc.nextInt();
 
 
 
 
 
-
-
-
-
-
+    sc.close();
+    }
+    public int hasId(List<Employee> list,int id){
+        for (int i = 0; i < list.size(); i++ ){
+            if (list.get(i).getId() == id) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
